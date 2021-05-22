007d4dc8e4387a94f8e653297444558c70a90268
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2021-05-22 14:22:30 +0200

    Add files via upload

---
+++ zgloszenie.php
@@ -0,0 +1,19 @@
+
+  <?php
+
+      $numer = $_POST['numer'];
+      $nrdysp = $_POST['nrdysp'];
+      $adres = $_POST['adres'];
+
+
+      $baza=mysqli_connect('localhost','root','','ratownictwo');
+      if(mysqli_connect_errno())
+      {echo"wystąpił bląd połączenia z baza";}
+      $wynik=mysqli_query($baza, "INSERT INTO `zgloszenia` VALUES (NULL,'$numer','$nrdysp','$adres',0,NOW())");
+          
+      
+      echo "<center><h1> Zgłoszenie zostało dodane</h1> </center>";
+ 
+       
+      mysqli_close($baza);
+      ?>
